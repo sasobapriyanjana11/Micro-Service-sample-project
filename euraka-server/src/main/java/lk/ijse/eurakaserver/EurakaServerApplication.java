5dd5dd836242f150b92a8ccae6a5e4b5843cf0ee
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2025-02-19 09:44:54 +0530

    initial project

---
+++ euraka-server/src/main/java/lk/ijse/eurakaserver/EurakaServerApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.eurakaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurakaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurakaServerApplication.class, args);
+	}
+
+}
---
+++ euraka-server/src/test/java/lk/ijse/eurakaserver/EurakaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.eurakaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurakaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
